babc69d14ba432f9828a0bfb910ae66bf1241500
zed-chi <zed-chi@mail.ru>
2023-02-21 16:23:06 +0300

    lint

--- search/views.py
+++ search/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render
-from products.models import Product
-from django.views.generic import ListView
 from django.db.models import Q
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from products.models import Product
+
 
 class SearchProductListView(ListView):
     template_name = "search/list.html"
-    
+
     def get_queryset(self, *args, **kwargs):
         request = self.request
         query = request.GET.get("query", None)
-        if query:            
+        if query:
             return Product.objects.search(query)
-
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
