7da5567d4ecccc39920e50cf1e7ee882093a6e63
zed-chi <zed-chi@mail.ru>
2023-02-21 22:18:12 +0300

    cart model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from products.models import Product
+
+User = settings.AUTH_USER_MODEL
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, blank=True)
+    total = models.DecimalField(default=0, decimal_places=2, max_digits=10)
