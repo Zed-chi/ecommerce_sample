cc1a7b19716dedc34931c56ca6ff1a2d4ce6d2fe
zed-chi <zed-chi@mail.ru>
2023-02-21 16:23:36 +0300

    tag model

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.db.models.signals import post_save, pre_save
 
-# Create your models here.
+from products.models import Product
+from products.util import unique_slug_generator
+
+
+class Tag(model.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField()
+    active = models.BooleanField()
+    products = models.ManyToManyField(Product, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+def tah_pre_save_receiver(sender, instance, *atgs, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+
+pre_save.connect(product_pre_save_receiver, sender=Tag)
