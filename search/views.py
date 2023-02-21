4b7f88799dc2f8ae63b07b570d8e8d96d02a1ca8
zed-chi <zed-chi@mail.ru>
2023-02-21 16:11:19 +0300

    search view

--- search/views.py
+++ search/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from products.models import Product
 from django.views.generic import ListView
+from django.db.models import Q
 
 class SearchProductListView(ListView):
     template_name = "search/list.html"
+    
     def get_queryset(self, *args, **kwargs):
         request = self.request
-        query = request.GET.get("query")
-        if query:
-            return Product.objects.filter(title__icontains=query)
+        query = request.GET.get("query", None)
+        if query:            
+            return Product.objects.search(query)
+
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["query"] = self.request.GET.get("query", None)
+        return context
