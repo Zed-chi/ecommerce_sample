6554bacc5f9631c562418b2c991001813fe5b822
zed-chi <zed-chi@mail.ru>
2023-02-21 22:18:30 +0300

    basic view

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# Create your models here.
+def cart_home(req):
+    req.session["cart_id"] = req.user
+    req.session["user"] = req.user.username
+    return render(req, "cart/home.html")
