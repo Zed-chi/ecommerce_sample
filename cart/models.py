d24a1f9b94df04f5d356a4048cd10fcee06b1c9a
zed-chi <zed-chi@mail.ru>
2023-02-22 15:52:58 +0300

    added signals

--- cart/models.py
+++ cart/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-
+from django.db.models.signals import pre_save, post_save, m2m_changed
 from products.models import Product
 
 User = settings.AUTH_USER_MODEL
@@ -17,3 +17,24 @@ class Cart(models.Model):
     total = models.DecimalField(default=0, decimal_places=2, max_digits=10)
 
     objects = CartManager()
+
+
+def pre_save_cart_receiver(sender, instance, action, *args, **kwargs):
+    if action in ["post_add", "post_remove", "post_clear"]:
+        products = instance.products.all()
+        total = sum(map(lambda p:p.price, products))
+        instance.total = total
+        instance.save()
+
+
+def m2m_save_cart_receiver(sender, instance, action, *args, **kwargs):
+    if action in ["post_add", "post_remove", "post_clear"]:
+        products = instance.products.all()
+        total = sum(map(lambda p:p.price, products))
+        instance.total = total
+        instance.save()
+
+
+pre_save.connect(pre_save_cart_receiver, Cart.products.through)
+m2m_changed.connect(m2m_save_cart_receiver, Cart.products.through)
+
