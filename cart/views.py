268ec4aa56b1c2694587fd882246bcdebea09634
zed-chi <zed-chi@mail.ru>
2023-02-22 15:53:13 +0300

    added update view

--- cart/views.py
+++ cart/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Cart
+from products.models import Product
+
+
+def get_cart(req):
+    cart_id = req.session.get("cart_id", None)    
+    if not cart_id:        
+        cart = Cart.objects.create(user=req.user)
+        req.session["cart_id"] = cart.id
+        print(f"=== {cart}")  
+    else:        
+        cart = Cart.objects.get(pk=cart_id)
+        print(f"=== {cart} exists")  
+    return cart
 
 
 def cart_home(req):
-    cart_id = req.session.get("cart_id", None)
-    if not cart_id:        
-        cart = Cart.objects.create(user=req.user)
-        req.session["cart_id"] = cart.id        
-    else:        
-        cart = Cart.objects.get(pk=cart_id)
-    return render(req, "cart/home.html")
+    cart = get_cart(req)
+    print(f"=== {cart} got")  
+    return render(req, "cart/home.html", {"cart":cart})
+
+
+def cart_update(req):
+    product_id = req.GET.get("product_id")
+    product = Product.objects.get(pk=product_id)    
+    cart = get_cart(req)
+    if product not in cart.products:
+        cart.products.add(product)  
+    else:
+        cart.products.remove(product)
+    req.session["cart_items"] = cart.products.count()  
+    return redirect("cart:home")
