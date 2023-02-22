85901ba7bbeb46643572b3c242d867d91289d656
zed-chi <zed-chi@mail.ru>
2023-02-22 11:47:02 +0300

    cart model manager

--- cart/models.py
+++ cart/models.py
@@ -6,7 +6,14 @@ from products.models import Product
 User = settings.AUTH_USER_MODEL
 
 
+class CartManager(models.Manager):
+    def new_cart(self, user=None):
+        return self.model.objects.create(user=user)
+    
+
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, blank=True)
     total = models.DecimalField(default=0, decimal_places=2, max_digits=10)
+
+    objects = CartManager()
